e86bbee653391219066bc73003407db125441cab
reinhardd <reinhard.doerner@googlemail.com>
2018-07-02 12:45:58 +0200

    Update dush.sh
    less verbose server list

--- dush.sh
+++ dush.sh
@@ -28,11 +28,6 @@ WORKDIR=$PWD
 # hosts.list:
 # HOSTLIST="--add-host=svn-server:10.100.250.15 ... "
 #
-#        --add-host=svn-server:10.100.250.15 \
-#        --add-host=svn-server.local:10.100.250.15 \
-#        --add-host=cvs-server:10.100.250.15 \
-#        --add-host=cvs-server.local:10.100.250.15 \
-#        --add-host=gitlab.krauth-eb.local:10.100.250.203 \
 if [ -f hosts.list ]; then
     source hosts.list
 fi
