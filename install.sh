07fc9f5d4d817f3b6d75d8aecd120ef405ac71ec
Reinhard Dörner <reinhard.doerner@krauth-online.de>
2018-11-12 11:04:56 +0100

    dush_create.sh renamed to create_dush.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 install -d /usr/share/dush
 install -d /usr/share/dush/stretch
 install stretch/Dockerfile /usr/share/dush/stretch/
-install dush_create.sh /usr/bin/
+install create_dush.sh /usr/bin/
 install dush.sh /usr/bin/
