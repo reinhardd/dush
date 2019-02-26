de2fcc749cf765b7c7e73f86e3fc5a30f75b4dce
Reinhard Dörner <reinhard.doerner@krauth-online.de>
2019-02-26 15:39:19 +0100

    add jessie

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 install -d /usr/share/dush
 install -d /usr/share/dush/stretch
+install -d /usr/share/dush/jessie
 install stretch/Dockerfile /usr/share/dush/stretch/
+install jessie/Dockerfile /usr/share/dush/jessie/
 install create_dush.sh /usr/bin/
 install dush.sh /usr/bin/
